152282829be37235e0fa0d1b1fec4336cd3a21fc
Krina Soni <krina.soni@openxcellinc.com>
2019-09-09 17:22:56 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Header from './components/header';
+
+const client = new ApolloClient({
+    uri: "http://localhost:3000/graphql"
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div className="App">
+                <Header />
+                <Query
+                    query={gql`
+      {
+        getAllCompanies {
+          _id
+          name
+          location
+        }
+      }
+    `} >
+                    {({ loading, error, data }) => {
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>Error :(</p>; return data.getAllCompanies.map(({ _id, name, location }) => (
+                            <div key={_id}>
+                                <p>{`${name} by ${location}`}</p>
+                            </div>
+                        ));
+                    }}
+                </Query>
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;
