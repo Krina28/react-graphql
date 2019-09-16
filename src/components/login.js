b5b4a2c9c6755db54fe7c7aa0c7038812d1dacfb
Krina Soni <krina.soni@openxcellinc.com>
2019-09-16 17:45:44 +0530

    form data fetching

--- src/components/login.js
+++ src/components/login.js
@@ -1,88 +1,113 @@
 import React from 'react';
 import { Formik } from 'formik';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import FormControl from '@material-ui/core/FormControl';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import gql from "graphql-tag";
+import { useQuery } from '@apollo/react-hooks';
 
-const useStyles = makeStyles(theme => ({
-    margin: {
-        margin: theme.spacing(1),
-    },
-}));
-
-const Login = () => {
-    const classes = useStyles();
-    return (
-        <div>
-            <Formik
-                initialValues={{ email: '', password: '' }}
-                validate={values => {
-                    let errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        (false);
-                    }, 400); setSubmitting
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleSubmit,
-                    isSubmitting,
-                }) => (
-                        <FormControl className={classes.margin}>
-                            <form onSubmit={handleSubmit}>
-                                <Grid container spacing={1} alignItems="flex-end">
-                                    <Grid item>
-                                        <AccountCircle />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField type="email"
-                                            label="Email"
-                                            onChange={handleChange}
-                                            value={values.email}
-                                        />
-                                    </Grid>
-                                </Grid>
-                                {errors.email && touched.email && errors.email}
-                                <Grid container spacing={1} alignItems="flex-end">
-                                    <Grid item>
-                                        <AccountCircle />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField type="password"
-                                            label="Password"
-                                            onChange={handleChange}
-                                            value={values.password}
-                                        />
-                                    </Grid>
-                                </Grid>
-                                {errors.password && touched.password && errors.password}
-                                <Button type="submit" variant="outlined" color="primary"
-                                    className={classes.button} disabled={isSubmitting}>
-                                    Login
-      </Button>
-                            </form>
-                        </FormControl>
-                    )}
-            </Formik>
-        </div>
-    )
+const defaultProps = {
+    bgcolor: 'background.paper',
+    borderColor: 'text.primary',
+    m: 1,
+    border: 1,
+    padding: 5
 };
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formValues: []
+        }
+    }
+
+    render() {
+        console.log(this.state)
+        const { formValues: { email, password } } = this.state;
+        const LOGIN = gql`
+        mutation login($email: String!, password: String!) {
+            login(email: $email, password: $password) {
+              _id
+              token
+            }
+        }
+        `;
+
+        const { loading, error, data } = useQuery(LOGIN, { variables: { email, password } });
+        console.log('???login', loading, error, data)
+
+        return (
+            <div>
+                <Box borderRadius={16} {...defaultProps}>
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+                        validate={values => {
+                            let errors = {};
+                            if (!values.email) {
+                                errors.email = 'Required';
+                            } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                            ) {
+                                errors.email = 'Invalid email address';
+                            }
+                            return errors;
+                        }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            console.log('values', values)
+                            this.setState({ formValues: values })
+                            setSubmitting(values)
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleSubmit,
+                        }) => (
+                                <FormControl>
+                                    <form onSubmit={handleSubmit}>
+                                        <Grid container spacing={1} alignItems="flex-end">
+                                            <Grid item>
+                                                <AccountCircle />
+                                            </Grid>
+                                            <Grid item>
+                                                <TextField type="email"
+                                                    name="email"
+                                                    label="Email"
+                                                    onChange={handleChange}
+                                                    value={values.email}
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                        {errors.email && touched.email && errors.email}
+                                        <Grid container spacing={1} alignItems="flex-end">
+                                            <Grid item>
+                                                <AccountCircle />
+                                            </Grid>
+                                            <Grid item>
+                                                <TextField type="password"
+                                                    name="password"
+                                                    label="Password"
+                                                    onChange={handleChange}
+                                                    value={values.password}
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                        {errors.password && touched.password && errors.password}
+                                        <Button type="submit" variant="outlined" color="primary">Login</Button>
+                                    </form>
+                                </FormControl>
+                            )}
+                    </Formik>
+                </Box>
+            </div>
+        )
+    }
+}
 
 export default Login;
